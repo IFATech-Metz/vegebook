1a62535fda8932de9a5aac3bd0d7ab56af09ef31
irixlevrai <kircher.franck@stagiairesifa.fr>
2018-11-16 15:58:50 +0100

    commit initial

---
+++ test.php
@@ -0,0 +1,52 @@
+
+
+<!DOCTYPE HTML>
+<head>
+        <script src="https://ajax.googleapis.com/ajax/libs/jquery/3.3.1/jquery.min.js"></script>
+        
+        
+        
+        <script type="text/javascript" src="DataTables/media/js/jquery.dataTables.min.js"></script>
+        <script type="text/javascript" src="tableau.js"></script>
+        <link rel="stylesheet" type="text/css" href="DataTables/media/css/jquery.dataTables.min.css">
+         
+    
+    <script type="text/javascript" src="https://cdnjs.cloudflare.com/ajax/libs/twitter-bootstrap/4.0.0-alpha.2/js/umd/tooltip.js"></script>
+    
+    
+    
+    
+    
+    <script type="text/javascript" src='DataTables/media/js/jquery.js'></script>
+     <script type="text/javascript" src="https://unpkg.com/popper.js"></script>
+    <link rel="stylesheet" href="https://stackpath.bootstrapcdn.com/bootstrap/4.1.3/css/bootstrap.min.css" integrity="sha384-MCw98/SFnGE8fJT3GXwEOngsV7Zt27NXFoaoApmYm81iuXoPkFOJwJ8ERdknLPMO" crossorigin="anonymous">
+
+    <script src="https://code.jquery.com/jquery-3.3.1.slim.min.js" integrity="sha384-q8i/X+965DzO0rT7abK41JStQIAqVgRVzpbzo5smXKp4YfRvH+8abtTE1Pi6jizo" crossorigin="anonymous"></script>
+<script src="https://cdnjs.cloudflare.com/ajax/libs/popper.js/1.14.3/umd/popper.min.js" integrity="sha384-ZMP7rVo3mIykV+2+9J3UJ46jBk0WLaUAdn689aCwoqbBJiSnjAK/l8WvCWPIPm49" crossorigin="anonymous"></script>
+<script src="https://stackpath.bootstrapcdn.com/bootstrap/4.1.3/js/bootstrap.min.js" integrity="sha384-ChfqqxuZUCnJSK3+MXmPNIyE6ZbWh2IMqE241rYiqJxyMiZ6OW/JmZQ5stwEULTy" crossorigin="anonymous"></script>
+    
+       <script type="text/javascript" src="https://cdn.jsdelivr.net/npm/bootstrap-confirmation2/dist/bootstrap-confirmation.min.js"></script>
+ 
+</head>
+
+
+
+
+<body>
+    
+
+    
+    
+    <form action="delete-traitement.php" method="post">
+<p>
+    <input type="text" name="id">
+    <input type="submit" value="envoyer">
+</p>
+</form>
+    
+<center><button class="btn btn-default" data-toggle="confirmation" data-placement="right">Confirmation on right</button>
+</center>
+
+</body>
+
+    
